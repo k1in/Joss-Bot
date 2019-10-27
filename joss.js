ce7c094431c2e4147ba9c6046711982b15a433bb
k1in <krk.tony@gmail.com>
2019-10-27 14:19:30 +0200

    Joss Bot

--- joss.js
+++ joss.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
 const fs = require("fs")
+const db = require("quick.db")
 bot.commands = new Discord.Collection()
 bot.aliases = new Discord.Collection()
 let coins = require("./coins.json")
